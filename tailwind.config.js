6e05a3a55f4e3cb3301f35d89f7832f75ea821dc
Sanjeev18207 <u.sanjeev.23cse@psvpec.in>
2024-02-29 10:24:16 +0530

    design: input

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        Purple: "#854dff",
+        Lightred: "#ff5757",
+        Offwhite: "#f0f0f0",
+        Lightgrey: "#dbdbdb",
+        Smokeygrey: "#716f6f",
+        Offblack: "#141414",
+      },
+    },
+  },
+  plugins: [],
+};
